a96b2b09b333af91734ff19edf0b1f0cdbb505d7
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 19:59:43 +0700

    feat: update Cart and CartItem models to use UUID for cart ID and enforce unique constraint on cart-item pairs

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -68,12 +70,16 @@ class Address(models.Model):
   customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
   
 class Cart(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4)
   created_at = models.DateTimeField(auto_now_add=True)
   
 class CartItem(models.Model):
-  cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+  cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
   quantity = models.PositiveIntegerField()
+  
+  class Meta:
+    unique_together = [['cart', 'product']]
   
 class Review(models.Model):
   product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
