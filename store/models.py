c8e21bb6238053a813c4ef3a32b0eba315457c53
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 11:01:07 +0700

    feat: revert store meta

--- store/models.py
+++ store/models.py
@@ -35,12 +35,6 @@ class Customer(models.Model):
   birth_date = models.DateField(null=True, blank=True)
   membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_B)
   
-  class Meta:
-    db_table = 'store_customers'
-    indexes = [
-      models.Index(fields=['last_name', 'first_name'])
-    ]
-  
 class Order(models.Model):
   PAYMENT_STATUS_PENDING = 'P'
   PAYMENT_STATUS_COMPLETE = 'C'
