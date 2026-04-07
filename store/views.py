901f17a775508008e325424397f093b128f1eab1
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 16:55:16 +0700

    feat: enhance models and serializers for collections and products, update views for detailed responses

--- store/views.py
+++ store/views.py
@@ -1,12 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-  return Response('ok')
+  queryset = Product.objects.select_related('collection').all()
+  serializer = ProductSerializer(queryset, many=True, context={'request': request})
+  return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
+  # try:
+  #   product = Product.objects.get(pk=id)
+  #   serializer = ProductSerializer(product)
+  #   return Response(serializer.data)
+  # except Product.DoesNotExist:
+  #   return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  product = get_object_or_404(Product, pk=id)
+  serializer = ProductSerializer(product, context={'request': request})
+  return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
   return Response('ok')
