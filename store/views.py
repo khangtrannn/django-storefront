8cf01308d6fac63b08135e75f2281cde728872f4
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 18:12:07 +0700

    feat: refactor ProductDetail and CollectionDetail views to use RetrieveUpdateDestroyAPIView and add deletion logic for collections

--- store/views.py
+++ store/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from .models import Collection, Product
@@ -21,18 +22,10 @@ class ProductList(ListCreateAPIView):
   def get_serializer_context(self):
     return {'request': self.request}
   
-class ProductDetail(APIView):
-  def get(self, request, id):
-    product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product, context={'request': request})
-    return Response(serializer.data)
-  
-  def put(self, request, id):
-    product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product, data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = 'id'
   
   def delete(self, request, id):
     product = get_object_or_404(Product, pk=id)
@@ -47,7 +40,16 @@ class CollectionList(ListCreateAPIView):
   queryset = Collection.objects.annotate(products_count=Count('product')).all()
   serializer_class = CollectionSerializer
   
-class CollectionDetail(ListCreateAPIView):
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
   queryset = Collection.objects.annotate(products_count=Count('product')).all()
   serializer_class = CollectionSerializer
+  
+  def delete(self, request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    
+    if collection.product_set.count() > 0:
+      return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    collection.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
   
