a40b399a0682c9126a499b5b6ca3fbfdb8dd53ea
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 10:55:45 +0700

    feature: implement initial database migrations and update models for store and tags

--- store/models.py
+++ store/models.py
@@ -10,8 +10,9 @@ class Collection(models.Model):
   
 class Product(models.Model):
   title = models.CharField(max_length=255)
+  slug = models.SlugField()
   description = models.TextField()
-  price = models.DecimalField(max_digits=6, decimal_places=2)
+  unit_price = models.DecimalField(max_digits=6, decimal_places=2)
   inventory = models.IntegerField()
   last_update = models.DateTimeField(auto_now=True)
   collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -33,6 +34,12 @@ class Customer(models.Model):
   phone = models.CharField(max_length=20)
   birth_date = models.DateField(null=True, blank=True)
   membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_B)
+  
+  class Meta:
+    db_table = 'store_customers'
+    indexes = [
+      models.Index(fields=['last_name', 'first_name'])
+    ]
   
 class Order(models.Model):
   PAYMENT_STATUS_PENDING = 'P'
