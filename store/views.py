5a0d8bf21817d22f15018824e332a9a6c0c84a09
Khang Tran <khangtrann8198@gmail.com>
2026-04-07 19:37:38 +0700

    feat: add ordering functionality to ProductViewSet for better product sorting

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
@@ -14,9 +14,10 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
-  filter_backends = [DjangoFilterBackend, SearchFilter]
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   filterset_class = ProductFilter
   search_fields = ['title', 'description']
+  ordering_fields = ['unit_price', 'last_update']
   
   def get_serializer_context(self):
     return {'request': self.request}
