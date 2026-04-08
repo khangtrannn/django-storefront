6a7340cd9e7a0706381ee3be2c7396f396a25d59
Khang Tran <khangtrann8198@gmail.com>
2026-04-08 18:05:51 +0700

    feat: implement templated email functionality with hello.html for personalized greetings

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,21 @@
 from django.http import JsonResponse
 from django.core.mail import mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 
 def hello_world(request):
   try:
     # mail_admins('Django', 'Hello, World!', html_message='<p>Hello, World!</p>')
-    message = EmailMessage('Django', 'Hello, World!', to=['khangtrann8198@gmail.com'])
-    message.attach_file('playground/static/images/inspectorio.jpeg') 
-    message.send()
+    
+    # message = EmailMessage('Django', 'Hello, World!', to=['khangtrann8198@gmail.com'])
+    # message.attach_file('playground/static/images/inspectorio.jpeg') 
+    # message.send()
+    
+    message = BaseEmailMessage(
+      template_name='emails/hello.html',
+      context={'name': 'Khang'},
+    )
+    
+    message.send(['khangtrann8198@gmail.com'])
   except BadHeaderError:
     pass
   
