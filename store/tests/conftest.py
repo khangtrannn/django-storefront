6c36ff8a1c998bf7dc9d4fa62aa230325ee60ee2
Khang Tran <khangtrann8198@gmail.com>
2026-04-09 10:35:06 +0700

    feat: refactor collection tests to use api_client fixture for improved readability and maintainability

---
+++ store/tests/conftest.py
@@ -0,0 +1,15 @@
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+import pytest
+
+
+@pytest.fixture
+def api_client():
+  return APIClient()
+
+@pytest.fixture
+def do_authenticate(api_client):
+  def do_authenticate(is_staff=False):
+    api_client.force_authenticate(user=User(is_staff=is_staff))
+    
+  return do_authenticate
